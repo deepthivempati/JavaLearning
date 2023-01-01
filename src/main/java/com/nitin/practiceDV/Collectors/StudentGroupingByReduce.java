e30bcd47a9cfc246c771e1f9be3d503e671c5742
deepthivempati <deepthi2135@gmail.com>
2023-01-01 22:10:14 +0530

    Collectors groupingBy, maxBy

--- src/main/java/com/nitin/practiceDV/Collectors/StudentGroupingByReduce.java
+++ src/main/java/com/nitin/practiceDV/Collectors/StudentGroupingByReduce.java
@@ -1,19 +1,22 @@
 package com.nitin.practiceDV.Collectors;
 
 import java.util.Comparator;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.BinaryOperator;
 
 import static com.nitin.practiceDV.Collectors.StudentPartitioningBy.students;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.reducing;
+import static java.util.stream.Collectors.*;
 
 // finding the student with the best and worst grade in respective city
 public class StudentGroupingByReduce {
     public static void main(String[] args) {
-        System.out.println(students
-                        .stream()
-                        .collect(groupingBy(Student::getCity),
-                reducing(BinaryOperator.maxBy(Comparator.comparing(Student::getAvgGrade))))
-        );
+        Map<String, Optional<Student>> collect = students
+                .stream()
+                .collect(
+                        groupingBy(Student::getCity, maxBy(Comparator.comparing(Student::getAvgGrade))
+                        )
+                );
+        //TODO: Optional<Student> to Student
     }
 }
