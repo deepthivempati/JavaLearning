53307a74f5508e294d0267b3ab731cacf5b507da
deepthivempati <deepthi2135@gmail.com>
2023-01-18 18:24:41 +0530

    BooleanUtils.toBooelan Boolean.valueOf

--- src/main/java/com/learn/practiceDV/BooleanUtilsConcept.java
+++ src/main/java/com/learn/practiceDV/BooleanUtilsConcept.java
@@ -6,26 +6,32 @@ public class BooleanUtilsConcept {
         String string1 = "true";
         String string2 = "on";
         String string3 = "yes";
+        String string4 = "no";
         Boolean result1 = Boolean.valueOf(firstName);
         Boolean result2 = Boolean.valueOf(string1);
         Boolean result3 = Boolean.valueOf(string2);
         Boolean result4 = Boolean.valueOf(string3);
+        Boolean result5 = Boolean.valueOf(string4);
         System.out.println("result1: "+result1);
         System.out.println("result2: "+result2);
         System.out.println("result3: "+result3);
         System.out.println("result4: "+result4);
+        System.out.println("result5: "+result5);
 
         System.out.println("******************************");
 
         String str1 = "true";
         String str2 = "on";
         String str3 = "yes";
-        Boolean result5 = BooleanUtils.toBoolean(str1);
-        Boolean result6 = BooleanUtils.toBoolean(str2);
-        Boolean result7 = BooleanUtils.toBoolean(str3);
-        System.out.println("result4: "+result5);
-        System.out.println("result5: "+result6);
-        System.out.println("result6: "+result7);
+        String str4 = "no";
+        Boolean result6 = BooleanUtils.toBoolean(str1);
+        Boolean result7 = BooleanUtils.toBoolean(str2);
+        Boolean result8 = BooleanUtils.toBoolean(str3);
+        Boolean result9 = BooleanUtils.toBoolean(str4);
+        System.out.println("result4: "+result6);
+        System.out.println("result5: "+result7);
+        System.out.println("result6: "+result8);
+        System.out.println("result6: "+result9);
 
     }
 }
